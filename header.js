ab3b4f9d3df842d5c30f6ad758de2549b42e4fc1
acc48 <acc48acc@gmail.com>
2025-11-24 17:38:01 +1100

    Update header.js

--- header.js
+++ header.js
@@ -1,4 +1,4 @@
 document.write('\
-<div id="header"><h2>48th Australasian Combinatorics Conference (ACC48)</h2></div>\
+<div id="header"><h2>48th Australasian Combinatorics Conference (48ACC)</h2></div>\
 <img id="frontphoto" src="melbourne.jpg" width="760">\
 ');
