51db432fa0e7ff139b4a9a593e96fd20dbea5e3b
davidwooddavid <43978638+davidwooddavid@users.noreply.github.com>
2026-08-03 09:38:09 +1000

    updates

--- sponsers.js
+++ sponsers.js
@@ -1,7 +1,8 @@
 document.write('\
 <center>\
-<img src="Monash-logo.jpg" height="80">\
-<img src="CMSALOGO.png" height ="120">\
-<img src="ica_logo.png" height ="110">\
+<a href="https://www.monash.edu/science/schools/school-of-mathematics"><img src="Monash-logo.jpg" height="80" style="margin-left: 0px; margin-right: 5px;" ></a>\
+<a href="http://combinatorics-australasia.org"><img src="CMSALOGO.png" height ="120" style="margin-left: 0px; margin-right: 10px;" ></a>\
+<a href="https://the-ica.org/"><img src="ica_logo.png" height ="110" style="margin-left: 10px; margin-right: 10px;" ></a>\
+<a href="https://www.tccpublishing.org"><img src="tcclogo.png" height ="110" style="margin-left: 10px; margin-right: 0px;" ></a>\
 </center>\
 ');
